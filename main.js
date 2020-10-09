c56e1f4b72fca84b6ea04bc13c2b82c33c7cea9c
Tetiana-Yarmolenko <kylik_t_86_86@ukr.net>
2020-10-09 16:59:43 +0300

    hw-8

--- main.js
+++ main.js
@@ -1,24 +1,90 @@
 import images from "./gallery-items.js";
 
 const gallery = document.querySelector(".js-gallery");
+
 const cardsMarkup = createImagesCardMarkup(images);
 
+const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
+
+const lightBoxImage = document.querySelector(".lightbox__image");
+const lightBox = document.querySelector(".lightbox");
+const lightboxOverlay = document.querySelector(".lightbox__overlay");
+let index = 0;
+let elLastChild = 0;
+
+
 gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
 function createImagesCardMarkup(images) {
   return images
     .map(({ preview, original, description }) => {
-      return `  <li class="gallery__item">
-    <a
-      class="gallery__link"
-      href=${original}
-      <img
-        class="gallery__image"
-        src=${preview}
-        data-source=${original}
-        alt=${description}
-      />
-    </a>
-  </li>`;
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href=${original}
+  >
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
     })
     .join("");
+}
+
+gallery.addEventListener("click", openModalWindow);
+closeModalBtn.addEventListener("click", onCloseModalWindow);
+lightboxOverlay.addEventListener("click", onCloseModalWindow);
+
+function openModalWindow(event) {
+  window.addEventListener("keydown", onEscKeyCloseModal);
+
+  window.addEventListener('keydown', onChangePhoto);
+
+  event.preventDefault();
+  lightBox.classList.add("is-open");
+  lightBoxImage.src = event.target.dataset.source;
+}
+
+function onCloseModalWindow() {
+  window.removeEventListener("keydown", onEscKeyCloseModal);
+  window.removeEventListener('keydown', onChangePhoto);
+  lightBox.classList.remove("is-open");
+  lightBoxImage.src = "";
+}
+
+function onEscKeyCloseModal(event) {
+  console.log(event.code);
+  if (event.code === "Escape") {
+    onCloseModalWindow();
+  }
+}
+
+
+function onChangePhoto(e) {
+  const arrOfSrc = [];
+  const arrayOflightBoxImage = document.querySelectorAll('.gallery__link');
+  arrayOflightBoxImage.forEach(src => arrOfSrc.push(src.href));
+  
+  for (let i = 0; i < arrOfSrc.length; i +=1){
+    elLastChild = i;
+    if (lightBoxImage.src === arrOfSrc[i]) {
+      index = i;
+    }
+  }
+  if (e.code === 'ArrowRight') {
+    lightBoxImage.src = arrOfSrc[index +=1];
+    if (index >= arrOfSrc.length) {
+      lightBoxImage.src = arrOfSrc[0];
+    }
+  }
+  if (e.code === 'ArrowLeft') {
+    lightBoxImage.src = arrOfSrc[index -= 1];
+    if (index < 0) {
+      lightBoxImage.src = arrOfSrc[elLastChild];
+    }
+  }
 }
